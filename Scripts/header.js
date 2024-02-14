3e15dabc67bb6c1f0dbf0c4aa30439da1cde353f
Anthony Z <118206086+2valide@users.noreply.github.com>
2024-02-14 14:34:19 +0100

    nav hover et cv

--- Scripts/header.js
+++ Scripts/header.js
@@ -1,4 +1,23 @@
+// nav
 window.addEventListener('scroll', function() {
     let navbar = document.getElementById('navbar');
     navbar.classList.toggle('sticky', window.scrollY > 0);
   });
+
+
+// nav barre hover
+document.addEventListener('DOMContentLoaded', function () {
+  var navItems = document.querySelectorAll('.nav ul li a');
+
+  navItems.forEach(function (item) {
+      item.addEventListener('click', function () {
+          // Supprimer la classe "active" de tous les éléments
+          navItems.forEach(function (navItem) {
+              navItem.parentElement.classList.remove('active');
+          });
+
+          // Ajouter la classe "active" à l'élément cliqué
+          this.parentElement.classList.add('active');
+      });
+  });
+});
