0f6bcea9f4db5c3afd1239bb430664048dec2952
Anthony Z <AnthonyZhao93@gmail.com>
2024-03-01 16:17:44 +0100

    logo update

--- Scripts/Cv.js
+++ Scripts/Cv.js
@@ -3,22 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const downloadButton = document.querySelector('.download-button');
 
     cvImage.addEventListener('mouseover', function () {
-        cvImage.style.filter = 'blur(3px)';
+        // cvImage.style.filter = 'blur(3px)';
         downloadButton.style.display = 'block';
     });
 
     cvImage.addEventListener('mouseout', function () {
-        cvImage.style.filter = 'blur(0)';
+        // cvImage.style.filter = 'blur(0)';
         downloadButton.style.display = 'none';
     });
 
     downloadButton.addEventListener('mouseover', function () {
-        cvImage.style.filter = 'blur(3px)';
+        // cvImage.style.filter = 'blur(30px)';
         downloadButton.style.display = 'block';
     });
 
     downloadButton.addEventListener('mouseout', function () {
-        cvImage.style.filter = 'blur(0)';
-        downloadButton.style.display = 'none';
+        // cvImage.style.filter = 'blur(0)';
     });
 });
--- Scripts/logo.js
+++ Scripts/logo.js
@@ -2,9 +2,12 @@ const targetText = "hello world";
     const rotatingLetters = document.querySelectorAll('.rotating-letters');
     let currentIndex = 0;
 
-    function rotateText() {
-        rotatingLetters[currentIndex].textContent = targetText[currentIndex];
-        currentIndex = (currentIndex + 1) % targetText.length;
-    }
+    setTimeout (() => {
+        function rotateText() {
+            rotatingLetters[currentIndex].textContent = targetText[currentIndex];
+            currentIndex = (currentIndex + 1) % targetText.length;
+        }
 
-    setInterval(rotateText, 200); // Change letter every 200 milliseconds
+        setInterval(rotateText, 200);
+
+    },2000)
