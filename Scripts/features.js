22b879fb8580018c30fd07c7a3d90a82859afdf4
Anthony Z <118206086+2valide@users.noreply.github.com>
2024-04-01 22:16:31 +0200

    feature update

--- Scripts/features.js
+++ Scripts/features.js
@@ -1,11 +1,14 @@
 function showContent(id) {
     // alert("test");
     document.getElementById(id).style.display = "none";
-    document.getElementById("cardContentHover" + id .slice(-1)).style.display = "block";
+    // document.getElementById("cardContentHover" + id .slice(-1)).style.display = "block";
+    document.getElementById("cardContentHover" + id .slice(-2)).style.display = "block";
+    
 
 }
 
 function hideContent(id) {
     document.getElementById(id).style.display = "block";
-    document.getElementById("cardContentHover" + id .slice(-1)).style.display = "none";
+    // document.getElementById("cardContentHover" + id .slice(-1)).style.display = "none";
+    document.getElementById("cardContentHover" + id .slice(-2)).style.display = "none";
 }
